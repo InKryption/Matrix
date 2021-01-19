dcadba0148c2ab7cc663c019a74765782b064d1b
InKryption <inkryption07@gmail.com>
2021-01-19 19:10:10 +0100

    All the dots

---
+++ .make/Debug.mk
@@ -0,0 +1,27 @@
+COMPILER:=x86_64-w64-mingw32-g++-win32
+ROOT:=$(CURDIR)
+BIN:=bin
+SRC:=src
+BUILD:=Debug
+NAME:=MatrixTest
+ARG:=
+
+INCLUDE:=\
+$(ROOT)/$(SRC)
+
+LIBRARY:=
+LIBNAME:=
+
+LFLAG:=\
+-static
+
+CFLAG:=\
+-D_DEBUG \
+-std=gnu++17 \
+-g -Og \
+-Wall -Werror -Wpedantic \
+-Wno-error=pedantic \
+-Wno-error=unknown-pragmas \
+-Wno-error=unused-variable \
+-Wno-error=unused-but-set-variable \
+-ftemplate-depth=32000
